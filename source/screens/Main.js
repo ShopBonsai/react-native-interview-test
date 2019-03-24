22ba63d71c1e5700fc23a0ddd04d6bf66c80a51c
williamweixibi <williamwxbi@gmail.com>
2019-03-24 17:27:15 -0400

    Revert "Application Completed"
    This reverts commit a8a3b2b2b81c60b2f8c2b0ef25a3de92b1361d37.

--- source/screens/Main.js
+++ source/screens/Main.js
@@ -1,59 +1,12 @@
 import React, { Component } from "react"
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native"
-import propTypes from "prop-types"
-
-import { FEEDPAGE } from "."
-
-const images = {
-  frontpage: require("../images/Github.png"),
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#CBFAF2",
-  },
-  newContainer: {
-    flex: 1,
-    backgroundColor: "#111111",
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
-  logoContainer: {
-    alignItems: "center",
-    flexGrow: 1,
-    justifyContent: "center",
-  },
-  logo: {
-    width: 200,
-    height: 200,
-  },
-})
+import { Text, ScrollView } from "react-native"
 
 export default class Main extends Component {
-  handlePress = () => {
-    this.props.navigator.push({ screen: "interview.feedpage" })
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={this.handlePress}>
-          <View style={styles.logoContainer}>
-            <Image style={styles.logo} source={images.frontpage} />
-            <Text style={styles.welcome}>Press to Load Feed</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+      <ScrollView>
+        <Text>Title</Text>
+      </ScrollView>
     )
   }
-}
-
-Main.propTypes = {
-  navigator: propTypes.object,
 }
--- source/screens/index.js
+++ source/screens/index.js
@@ -1,10 +1,7 @@
 import Main from "./Main"
-import FeedPage from "./FeedPage"
 
 export const MAIN = "interview.main"
-export const FEEDPAGE = "interview.feedpage"
 
 export default {
   [MAIN]: Main,
-  [FEEDPAGE]: FeedPage,
 }
