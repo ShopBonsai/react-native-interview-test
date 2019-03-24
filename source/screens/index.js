a8a3b2b2b81c60b2f8c2b0ef25a3de92b1361d37
williamweixibi <williamwxbi@gmail.com>
2019-03-24 17:18:26 -0400

    Application Completed

--- source/screens/index.js
+++ source/screens/index.js
@@ -1,7 +1,10 @@
 import Main from "./Main"
+import FeedPage from "./FeedPage"
 
 export const MAIN = "interview.main"
+export const FEEDPAGE = "interview.feedpage"
 
 export default {
   [MAIN]: Main,
+  [FEEDPAGE]: FeedPage,
 }
