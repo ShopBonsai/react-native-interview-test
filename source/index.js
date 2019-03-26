a7f21a511ef16d04aa5a5c51513ad1a77a1231a8
Subhanc <schaud86@uwo.ca>
2019-03-26 04:53:04 -0400

    Designed Navbar

--- source/index.js
+++ source/index.js
@@ -2,21 +2,30 @@ import { Navigation } from "react-native-navigation"
 
 import withProviders from "./containers/withProviders"
 import screens, { MAIN, MOVIELIST } from "./screens"
+import { bonsai_colour } from './styles';
 
 class App {
   constructor() {
     this.registerScreens(screens)
     Navigation.startSingleScreenApp({
       screen: {
-        screen: MOVIELIST,
-        navigatorStyle: { navBarHidden: false },
-      },
-    })
+        screen: MOVIELIST, // Use movie list as initial screen
+        title: "Bonsai Movies",
+        navigatorStyle: {
+          navBarHidden: false, // show navbar
+          navBarBackgroundColor: bonsai_colour.blue, // Use bonsai colour
+          navBarTextColor: '#FFFFFF',
+          navBarTranslucent: true, // enable translucency
+          largeTitle: true // enable Large Title in iOS NavigationBar
+        }
+      }
+    });
   }
-  registerScreens = screensToRegister =>
+  registerScreens = screensToRegister => {
     Object.entries(screensToRegister).forEach(([key, Screen]) =>
       Navigation.registerComponent(key, () => withProviders(Screen)),
     )
+  }
 }
 
 export default new App()
