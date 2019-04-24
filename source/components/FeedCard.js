2935135b62e36f9e3e7dd346bb9739b2bfd81ee7
Creig Fortune <fortunephiri004@gmail.com>
2019-04-24 18:01:22 -0400

    defining propTypes

--- source/components/FeedCard.js
+++ source/components/FeedCard.js
@@ -3,6 +3,7 @@ import React from "react"
 import { View, Text, Image } from "react-native"
 import { Icon } from "react-native-elements"
 import moment from "moment"
+import PropTypes from 'prop-types'
 
 import { Card, CardSection } from "./index"
 
@@ -12,12 +13,13 @@ class FeedCard extends React.Component {
   }
 
   render() {
+    const { date, genre, imageUrl, title } = this.props
     return (
       <Card>
         <View style={styles.containerStyle}>
-          <Text style={styles.cardHeader}>{this.props.title}</Text>
+          <Text style={styles.cardHeader}>{title}</Text>
           <View style={{ width: 400, height: 400 }}>
-            <Image style={styles.imageStyle} source={{ uri: `${this.props.imageUrl}` }} />
+            <Image style={styles.imageStyle} source={{ uri: `${imageUrl}` }} />
           </View>
         </View>
         <CardSection>
@@ -31,15 +33,28 @@ class FeedCard extends React.Component {
               this.setState({ iconSelected: !this.state.iconSelected })
             }}
           />
-          <Text style={{alignSelf:"center"}}>Like</Text>
+          <Text style={{ alignSelf: "center" }}>Like</Text>
           <View style={{ flexDirection: "column", marginLeft: 10 }}>
-            <Text style={styles.footerText}>{moment(this.props.date).format("MMMM Do YYYY, h:mm:ss a")}</Text>
-            <Text style={styles.footerText}>{`Genre: ${this.props.genre}`}</Text>
+            <Text style={styles.footerText}>{moment(date).format("MMMM Do YYYY, h:mm:ss a")}</Text>
+            <Text style={styles.footerText}>{`Genre: ${genre}`}</Text>
           </View>
         </CardSection>
       </Card>
     )
   }
+}
+
+FeedCard.propTypes = {
+  date: PropTypes.string,
+  genre: PropTypes.string,
+  imageUrl: PropTypes.string,
+  title: PropTypes.string,
+}
+
+FeedCard.defaultProps = {
+  date: moment().format("MMMM Do YYYY"),
+  genre: "unkown",
+  title: "unkown title",
 }
 
 export { FeedCard }
