89775da55715670a95ebef6975a0522288288e6e
benTranCodes <bbentran@gmail.com>
2019-04-28 18:59:38 -0400

    ➕ Add redux-thunk

--- source/reduxStore.js
+++ source/reduxStore.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware, combineReducers } from "redux"
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import ReduxThunk from "redux-thunk";
 
-import env from "./env"
-import reducers from "./ducks"
+import env from "./env";
+import reducers from "./ducks";
 
-export const rootReducer = combineReducers(reducers)
-const developmentMiddleware = [require("redux-logger").createLogger({ collapsed: true })]
+export const rootReducer = combineReducers(reducers);
+const developmentMiddleware = [require("redux-logger").createLogger({ collapsed: true })];
 
-const middleware = [...(env.IS_DEVELOPMENT ? developmentMiddleware : [])]
-const store = createStore(rootReducer, {}, applyMiddleware(...middleware))
+const middleware = [...(env.IS_DEVELOPMENT ? developmentMiddleware : [])];
+const store = createStore(rootReducer, {}, applyMiddleware(...middleware, ReduxThunk));
 
-export default store
+export default store;
