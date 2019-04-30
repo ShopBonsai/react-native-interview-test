0606e922ce3c706ecade845db9ab1587a6d22203
benTranCodes <bbentran@gmail.com>
2019-04-29 23:51:06 -0400

    ⚡️ Update movieService parseMovieList

--- source/services/movieService.js
+++ source/services/movieService.js
@@ -12,7 +12,7 @@ const movieService = {
   },
 
   /**
-   * Returns an array of movie listings after parsing and removing movies with incomplete data
+   * Returns a parsed array of movie listings after removing movies with incomplete data and converting the URLs
    * @param {string} movies.title
    * @param {string} movies.genre
    * @param {number} movies.price
@@ -21,18 +21,17 @@ const movieService = {
    * @param {string} movies.date
    */
   parseMovieList(movies) {
-    return movies.map((movie) => {
-      const { title, genre, price, inventory, image, date } = movie;
-
-      if (title && genre && price && inventory && image && date) {
-        return {
-          ...movie,
-          image: stringService.convertHttpToHttps(movie.image),
-        };
-      } else {
-        return movie;
-      }
-    });
+    return movies
+      .filter((movie) => {
+        const { title, genre, price, inventory, image, date } = movie;
+        if (title && genre && image && date && price >= 0 && inventory >= 0) {
+          return movie;
+        }
+      })
+      .map((movie) => ({
+        ...movie,
+        image: stringService.convertHttpToHttps(movie.image),
+      }));
   },
 };
 
