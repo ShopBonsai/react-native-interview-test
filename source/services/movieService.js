a840180db5667eb3a276dfeb1a049095970c3de2
benTranCodes <bbentran@gmail.com>
2019-04-29 22:49:16 -0400

    💡 Comment movieService

--- source/services/movieService.js
+++ source/services/movieService.js
@@ -1,8 +1,37 @@
 import { apiSettings } from "../settings";
 
 const movieService = {
+  /**
+   * Returns an array of movie listings
+   * @param {number} skip - Number of movie listings to skip by
+   * @param {number} limit - Number of how many movie listings to retrieve at a time
+   */
   getMovieList(skip = 0, limit = 10) {
     return fetch(apiSettings.getMovieList(skip, limit)).then((response) => response.json());
+  },
+
+  /**
+   * Returns an array of movie listings after parsing and removing movies with incomplete data
+   * @param {string} movies.title
+   * @param {string} movies.genre
+   * @param {number} movies.price
+   * @param {number} movies.inventory
+   * @param {string} movies.image
+   * @param {string} movies.date
+   */
+  parseMovieList(movies) {
+    return movies.map((movie) => {
+      const { title, genre, price, inventory, image, date } = movie;
+
+      if (title && genre && price && inventory && image && date) {
+        return {
+          ...movie,
+          image: stringService.convertHttpToHttps(movie.image),
+        };
+      } else {
+        return movie;
+      }
+    });
   },
 };
 
