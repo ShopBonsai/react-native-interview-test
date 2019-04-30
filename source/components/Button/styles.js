25175941234d1d14429a97d044fd55bae93a3884
benTranCodes <bbentran@gmail.com>
2019-04-30 02:15:16 -0400

    ✨ Create Button

---
+++ source/components/Button/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  buttonText: {
+    textAlign: "center",
+    letterSpacing: 1,
+    fontWeight: "600",
+    paddingTop: 13,
+    paddingBottom: 13,
+  },
+});
+
+export { styles };
