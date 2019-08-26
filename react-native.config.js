966e1cc20d890586401c2846c8b52c3754e56b48
Falconiere R. Barbosa <falconieer@gmail.com>
2019-08-26 06:48:50 -0300

    The updgrade and the clearning up of package.json completed

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  project: {
+    ios: {},
+    android: {}, // grouped into "project"
+  },
+  dependencies: {
+    "local-rn-library": {
+      root: "/root/libraries",
+    },
+  },
+  assets: ["./assets/fonts/"], // stays the same
+}
