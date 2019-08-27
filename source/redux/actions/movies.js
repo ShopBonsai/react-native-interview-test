0ba60835e127513a54eb2ffa93b59bfbf568f4e0
Falconiere R. Barbosa <falconieer@gmail.com>
2019-08-27 02:10:23 -0300

    Starting with first reducer

--- source/redux/actions/movies.js
+++ source/redux/actions/movies.js
@@ -2,20 +2,24 @@ const START_MOVIE_REQUEST = 'START_MOVIE_REQUEST'
 const SUCCESS_MOVIE_REQUEST = 'SUCCESS_MOVIE_REQUEST'
 const ERROR_MOVIE_REQUEST = 'SUCCESS_MOVIE_REQUEST'
 
-
-
 export function fetch(skip=0,limit=10) {
-  
   return async dispatch => {
     try {
-      
     } catch (error) {
-      
     }
   }
 }
 
 const initialState = {}
-export default movieReducer(state=initialState,action) {
-
+export default function movieReducer(state = initialState, action) {
+  switch(action.type){
+    case START_MOVIE_REQUEST:
+      return state
+    case SUCCESS_MOVIE_REQUEST:
+      return state
+    case ERROR_MOVIE_REQUEST:
+      return state
+    default:
+      return state
+  }
 }
