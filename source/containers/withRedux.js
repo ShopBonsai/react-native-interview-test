ec773671f0456d221d7c147f525623fb7d369afd
Falconiere R. Barbosa <falconieer@gmail.com>
2019-08-27 01:59:36 -0300

    Fixing code styles

--- source/containers/withRedux.js
+++ source/containers/withRedux.js
@@ -1,4 +1,4 @@
-import { Provider } from "react-redux"
-import { nest, compose, withProps } from "recompose"
+import { Provider } from 'react-redux'
+import { nest, compose, withProps } from 'recompose'
 
 export default store => Child => nest(compose(withProps({ store }))(Provider), Child)
--- source/containers/withSafeArea.js
+++ source/containers/withSafeArea.js
@@ -1,18 +1,15 @@
-import React, { Component } from "react"
-import { SafeAreaView } from "react-native"
-import styled from "styled-components/native"
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import styled from 'styled-components/native'
 
 const SafeView = styled(SafeAreaView)`
   flex: 1;
 `
 
-export default Child =>
-  class extends Component {
-    render() {
-      return (
-        <SafeView>
-          <Child {...this.props} />
-        </SafeView>
-      )
-    }
-  }
+export default Child => (
+  props => (
+    <SafeView>
+      <Child {...props} />
+    </SafeView>
+  )
+)
