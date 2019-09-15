0d5731140ed1c19ce8c3c219d8a7881f28df2f4e
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2019-09-15 03:29:47 -0300

    Add background colors

--- ios/Interview/AppDelegate.m
+++ ios/Interview/AppDelegate.m
@@ -21,7 +21,7 @@
                                                    moduleName:@"interview"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.15f green:0.17f blue:0.24f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
