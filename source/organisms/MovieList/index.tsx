7dd9e2a1b20cc907eb7daac3ac4cf2425d7903e2
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2019-09-15 15:16:29 -0300

    Enhance MovieList Organism (props and comments)

--- source/organisms/MovieList/index.tsx
+++ source/organisms/MovieList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useMemo, useRef } from 'react';
 import { FlatList, ActivityIndicator, ListRenderItem } from 'react-native';
 
 import MovieCard from '../../molecules/MovieCard';
@@ -8,24 +8,59 @@ import { Container, ItemContainer, LoadingContainer } from './style';
 
 export interface Props {
   movies: Movie[];
+  favorites: Movie[];
   loading?: boolean;
   onEndReached?: () => void;
+  onFavorite?: (movie: Movie, isFavorite: boolean) => void;
 }
 
-const MovieList: React.FC<Props> = ({ loading, movies, onEndReached }) => {
+const MovieList: React.FC<Props> = ({
+  favorites,
+  loading,
+  movies,
+  onEndReached,
+  onFavorite,
+}) => {
+  // Cache favorite ids for further comparison
+  const favoriteIds = useMemo<string[]>(
+    () => favorites.map(favorite => favorite._id.$oid),
+    [favorites],
+  );
+
+  // Flag to prevent FlatList onEndReached from being called
+  // more than once during scroll momentum
   const reachedEndDuringMomentum = useRef<boolean>(false);
 
+  // Movie item renderer
   const renderItem: ListRenderItem<Movie> = ({ item }) => {
+    // Delegate movie favorited/unfavorited callback to parent
+    const handleFavorite = (isFavorite: boolean): void => {
+      if (onFavorite) {
+        onFavorite(item, isFavorite);
+      }
+    };
+
+    // Verifies if movie is included in favorites
+    const isFavorite: boolean = favoriteIds.includes(item._id.$oid);
+
     return (
       <ItemContainer>
-        <MovieCard title={item.title} image={item.image} />
+        <MovieCard
+          title={item.title}
+          image={item.image}
+          onFavorite={handleFavorite}
+          isFavorite={isFavorite}
+        />
       </ItemContainer>
     );
   };
 
+  // Footer Renderer
   const renderFooter = (): JSX.Element | null => {
+    // Do not render anything in case list is not in loading state
     if (!loading) return null;
 
+    // Else render activity indicator (list is in loading state)
     return (
       <LoadingContainer>
         <ActivityIndicator color="#1685fb" />
@@ -33,14 +68,19 @@ const MovieList: React.FC<Props> = ({ loading, movies, onEndReached }) => {
     );
   };
 
+  // onEndReached callback
   const handleEndReached = (): void => {
+    // If there is a callback to be called AND scroll is not during momentum
     if (onEndReached && !reachedEndDuringMomentum.current) {
+      // Set momentum to true and call parent callback;
       reachedEndDuringMomentum.current = true;
       onEndReached();
     }
   };
 
+  // onMomentumScrollBegin callback
   const handleMomentum = (): void => {
+    // Reset scroll momentum flag
     reachedEndDuringMomentum.current = false;
   };
 
