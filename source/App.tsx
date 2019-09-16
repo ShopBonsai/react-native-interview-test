81f4906c2789ecd84dac4139387a9df83501fa80
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2019-09-16 19:46:04 -0300

    Add DetailsBottomSheet Container

--- source/App.tsx
+++ source/App.tsx
@@ -6,12 +6,14 @@ import FlashMessage from 'react-native-flash-message';
 import Routes from './routes';
 import store, { persistor } from './store';
 import withSafeArea from './containers/HOCs/withSafeArea';
+import DetailsBottomSheet from './containers/DetailsBottomSheet';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <Routes />
+        <DetailsBottomSheet />
         <FlashMessage />
       </PersistGate>
     </Provider>
---
+++ source/containers/DetailsBottomSheet/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import CTAButton from '../../atoms/CTAButton';
+import MovieDetails from '../../organisms/MovieDetails';
+import BottomSheet from '../../organisms/BottomSheet';
+import DetailsTicketPicker from '../DetailsTicketPicker';
+import { ApplicationState } from '../../store/ducks';
+import { clearSelection, DetailsState } from '../../store/ducks/details';
+import { setTicket } from '../../store/ducks/cart';
+
+const DetailsBottomSheet: React.FC = () => {
+  // Get values from details store state
+  const { movie, ticket } = useSelector<ApplicationState, DetailsState>(
+    store => store.details,
+  );
+
+  // Get dispatcher
+  const dispatch = useDispatch();
+
+  // Dispatch action to clear selected movie on bottom sheet close
+  const handleClose = (): void => {
+    dispatch(clearSelection());
+  };
+
+  // Dispatch action to set the selected ticket to cart (and clear selection)
+  const handleSetTicket = (): void => {
+    dispatch(setTicket(ticket));
+    handleClose();
+  };
+
+  // Render add to cart lead button
+  const renderLeadButton = (): JSX.Element => {
+    return (
+      <CTAButton
+        iconName="shopping-cart"
+        disabled={!ticket}
+        onPress={handleSetTicket}
+      >
+        Add To Cart
+      </CTAButton>
+    );
+  };
+
+  // Render bottom sheet content
+  const renderContent = (): JSX.Element | null => {
+    // Prevent render if there is no movie selected
+    if (!movie) {
+      return null;
+    }
+
+    return (
+      <MovieDetails movie={movie} lead={renderLeadButton()}>
+        <DetailsTicketPicker />
+      </MovieDetails>
+    );
+  };
+
+  return (
+    <BottomSheet visible={!!movie} onClose={handleClose}>
+      {renderContent()}
+    </BottomSheet>
+  );
+};
+
+export default DetailsBottomSheet;
