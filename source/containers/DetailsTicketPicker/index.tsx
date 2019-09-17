1f572098d840c5b559a7b287d0bcd69813e7c2ae
Guilherme Eduardo <guilherme.eduardo92@hotmail.com>
2019-09-16 21:19:59 -0300

    Enhance DetailsTicketPicker (load form cart)

--- source/containers/DetailsTicketPicker/index.tsx
+++ source/containers/DetailsTicketPicker/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import TicketAmountPicker from '../../organisms/TicketAmountPicker';
 import { ApplicationState } from '../../store/ducks';
 import { DetailsState, selectTicket } from '../../store/ducks/details';
+import { CartState } from '../../store/ducks/cart';
 import Ticket from '../../models/ticket';
 
 const DetailsTicketPicker: React.FC = () => {
@@ -12,8 +13,28 @@ const DetailsTicketPicker: React.FC = () => {
     store => store.details,
   );
 
+  // Get values from tickets cart state
+  const { tickets } = useSelector<ApplicationState, CartState>(
+    store => store.cart,
+  );
+
   // Get dispatcher
   const dispatch = useDispatch();
+
+  // Get amount of the ticket from cart upon mount
+  useEffect(() => {
+    if (!movie) {
+      return;
+    }
+
+    const cartTicket = tickets.find(currentCartTicket => {
+      return currentCartTicket.movie._id.$oid === movie._id.$oid;
+    });
+
+    if (cartTicket) {
+      dispatch(selectTicket(cartTicket));
+    }
+  }, [movie, tickets, dispatch]);
 
   // Prevent render if movie is not set
   if (!movie) {
